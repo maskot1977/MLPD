78802e30c656d82d2a546e8b0c046bc2355f5e5f
Masaaki Kotera <kotera@preferred.jp>
2021-05-10 13:02:54 +0900

    mlpdr/preprocess.py

--- mlpdr/preprocess.py
+++ mlpdr/preprocess.py
@@ -3,7 +3,7 @@ from sklearn import model_selection
 
 
 def train_test_split(X, Y, test_size=0.5):
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size)
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(X, Y, test_size)
     X_train = torch.from_numpy(x_train).float()
     X_test = torch.from_numpy(x_test).float()
     Y_train = torch.from_numpy(y_train).float()
--- tests/tests.py
+++ tests/tests.py
@@ -3,3 +3,4 @@ import pytest
 
 def test_ok():
     return True
+
