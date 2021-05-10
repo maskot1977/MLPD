cf2031cda73032e0b7177a6de29df70a92521aec
Masaaki Kotera <kotera@preferred.jp>
2021-05-10 13:58:15 +0900

    xavier

--- mlpdr/mlpdr.py
+++ mlpdr/mlpdr.py
@@ -8,6 +8,8 @@ class MLPDR(nn.Module):
         self.l2 = nn.Linear(n_hidden, n_output)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p_dropout)
+        nn.init.xavier_normal_(self.l1.weight)
+        nn.init.xavier_normal_(self.l2.weight)
 
     def forward(self, x):
         x = self.relu(self.l1(x))
